3c4bd998dfe09fd57c611980dd0c2b5fbe0a80fb
Geoffrey Parker <grp@gparker.co>
2013-04-08 16:22:06 -0500

    popover show/hide seems to work in all cases

--- chrome/trelloGithub.js
+++ chrome/trelloGithub.js
@@ -1,12 +1,12 @@
-var trelloGithub = (function($, Handlebars) {
+var trelloGithub = (function($) {
   var exports = {};
 
-  var popoverOffset;
+  var popoverOffset = {top: 581, left: 1123}; //default value, should change before showing.
 
   var jsGithubIssue = exports.jsGithubIssue = function() {
     var popover = $('#github-popover');
     popover.toggle();
-    popover.offset(popoverOffset);
+    //popover.offset(popoverOffset);
   }
 
   var addButton = exports.addButton = function() {
@@ -17,10 +17,21 @@ var trelloGithub = (function($, Handlebars) {
     link.click(jsGithubIssue);
     popoverOffset = link.offset();
     popoverOffset.top += link.height() + 18;
-
-    popover_html = Handlebars.templates.popover(popoverOffset);
-    $('body').append(popover_html);
   }
 
+  function setupPopover {
+    popover_html = '<div id="github-popover" class="pop-over clearfix fancy-scrollbar" style="left:' + popoverOffset.left + 'px; top: ' + popoverOffset.top + 'px; max-height: 891px; display: none;"></div>';
+    $('body').append(popover_html);
+    $('#github-popover').load(chrome.extension.getURL('popover.html'));
+    $('.js-close-github-popover').click(function(e){$('#github-popover').hide();});
+    $('body').click(function(e){
+      if ($(e.target).parents().index($('#github-popover') == -1)) {
+        $('#github-popover').hide();
+      }
+    });
+    $('.button-link:not(.js-github-issue)').click(function(e){$('#github-popover').hide();});
+  }
+
+
   return exports;
-})(jQuery, Handlebars);
+})(jQuery);
