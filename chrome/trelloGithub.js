f539e3fb83b361695117be6658a15874bd6b42b8
Geoffrey Parker <grp@gparker.co>
2013-04-09 12:19:01 -0500

    submit click handler authorizing with trello

--- chrome/trelloGithub.js
+++ chrome/trelloGithub.js
@@ -1,4 +1,4 @@
-var trelloGithub = (function($) {
+var trelloGithub = (function($, Trello) {
   var exports = {};
 
   var popoverOffset = {top: 581, left: 1123}; //default value, should change before showing.
@@ -19,23 +19,31 @@ var trelloGithub = (function($) {
     popoverOffset.top += link.height() + 18;
   }
 
+  var createIssue = exports.createIssue = function(event) {
+    Trello.authorize({type:"popup", scope:{read:true, write:true, account:false}, name:"Trello-Github", success:function(){
+      console.log('authorized');
+    }});
+  }
+
   function setupPopover() {
     popover_html = '<div id="github-popover" class="pop-over clearfix fancy-scrollbar" style="left:' + popoverOffset.left + 'px; top: ' + popoverOffset.top + 'px; max-height: 891px; display: none;"></div>';
     $('body').append(popover_html);
-    $('#github-popover').load(chrome.extension.getURL('popover.html'));
-    $('.js-close-github-popover').click(function(e){$('#github-popover').hide();});
-    $('body').click(function(e) {
-      if ($(e.target).parents().index($('#github-popover') == -1)) {
-        $('#github-popover').hide();
-      }
+    $('#github-popover').load(chrome.extension.getURL('popover.html'), null, function(){
+      $('.js-close-github-popover').click(function(e){$('#github-popover').hide();});
+      $('body').click(function(e) {
+        if ($(e.target).parents().index($('#github-popover') == -1)) {
+          $('#github-popover').hide();
+        }
+      });
+      $('.button-link:not(.js-github-issue)').click(function(e){$('#github-popover').hide();});
+      $('.js-create-github-issue').click(createIssue);
     });
-    $('.button-link:not(.js-github-issue)').click(function(e){$('#github-popover').hide();});
   }
 
   setupPopover();
 
   return exports;
-})(jQuery);
+})(jQuery, Trello);
 
 ob = new MutationObserver(function(objs, observer){
   var wasHidden = $.inArray('visibility: hidden', $.map(objs, function(o){return o['oldValue'];}).join().split(/;,? ?/)) != -1;
@@ -44,4 +52,5 @@ ob = new MutationObserver(function(objs, observer){
   }
 });
 
+//trigger ob's callback every time the style attribute on window changes
 ob.observe($('.window')[0], {attributes:true, attributeFilter:['style'], attributeOldValue:true})
